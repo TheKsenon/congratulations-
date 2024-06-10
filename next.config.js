6ad4305f8b1a7643f2b22d7bb0ca9832008b728a
Ksenon <139338488+TheKsenon@users.noreply.github.com>
2024-06-10 13:57:49 +0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true, 
+  // ... другие ваши настройки
+  trailingSlash: true // добавляет / к url
+};
+
+module.exports = nextConfig
